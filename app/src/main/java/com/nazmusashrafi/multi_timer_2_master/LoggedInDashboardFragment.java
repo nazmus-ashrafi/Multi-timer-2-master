f7ec9edd13bdcded610b3d77b2fde5b7b4c85559
Nazmus Ashrafi <nazmus.s.ashrafi@gmail.com>
2020-10-27 19:26:01 +0600

    update 3

--- app/src/main/java/com/nazmusashrafi/multi_timer_2_master/LoggedInDashboardFragment.java
+++ app/src/main/java/com/nazmusashrafi/multi_timer_2_master/LoggedInDashboardFragment.java
@@ -3,17 +3,33 @@ package com.nazmusashrafi.multi_timer_2_master;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class LoggedInDashboardFragment extends Fragment {
 
+    private DatabaseReference reference;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private String onlineUserID;
+
     private Button addBtn;
+    private TextView nameText;
 
 
     @Override
@@ -23,11 +39,20 @@ public class LoggedInDashboardFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_logged_in_dashboard, container, false);
 
 
+
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference().child("Users");
+        onlineUserID = mUser.getUid();
+
+
+        nameText = view.findViewById(R.id.nameText);
+
 
         view.findViewById(R.id.addButton).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +64,25 @@ public class LoggedInDashboardFragment extends Fragment {
             }
         });
 
+
+
+        reference.child(onlineUserID).child("name").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if(dataSnapshot.exists()){
+
+                    nameText.setText(dataSnapshot.getValue().toString()+ " ");
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
     }
